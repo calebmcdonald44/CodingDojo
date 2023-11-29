ef166a042a017d40bb02ef8487fd9e1d5b5a7150
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2023-11-29 17:35:30 -0600

    added a button that increments age

--- javaScript/react/propItUp/src/PersonCard.jsx
+++ javaScript/react/propItUp/src/PersonCard.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 const People = (props) => {
     const {firstName, lastName, age, hairColor} = props
+    let [currentAge, setCurrentAge] = useState(age)
+    const birthdayTick = () => setCurrentAge((previousAge) => previousAge+1)
+
     return (
         <>
             <h1>{lastName}, {firstName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {currentAge}</p>
             <p>Hair Color: {hairColor}</p>
+            <button onClick={birthdayTick}>Birthday Button for {firstName} {lastName}</button>
         </>
     );
 }
