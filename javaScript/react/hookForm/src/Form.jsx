bc847661793930fcfc82b7066baebe469283cbdc
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2023-11-30 16:17:06 -0600

    adding validations (not DRY)

--- javaScript/react/hookForm/src/Form.jsx
+++ javaScript/react/hookForm/src/Form.jsx
@@ -8,7 +8,14 @@ const Form = (props) => {
         lastName: "",
         email: "",
         password: "",
-        confirm: "",
+        confirm: ""
+    })
+
+    const [validations, setValidations] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
     })
 
     const createUser = (e) => {
@@ -18,8 +25,48 @@ const Form = (props) => {
         console.log('newUser: ', newUser);
         
     }
+    const errorChecker = (e) => {
+        if (e.target.name ==='firstName') {
+            if(e.target.value.length < 2){
+                setValidations((prevValidations) => ({...prevValidations, "firstName": "First name must be at least 2 characters."}))
+            } else {
+                setValidations((prevValidations) => ({...prevValidations, "firstName": ""}))
+            }
+        }
+        if (e.target.name === 'lastName') {
+            if(e.target.value.length < 2) {
+                setValidations((prevValidations) => ({...prevValidations, "lastName": "Last name must be at least 2 characters."}))
+            } else {
+                setValidations((prevValidations) => ({...prevValidations, "lastName": ""}))
+            }
+        }
+        if (e.target.name === 'email') {
+            if(e.target.value.length < 2) {
+                setValidations((prevValidations) => ({...prevValidations, "email": "Email must be at least 2 characters."}))
+            } else {
+                setValidations((prevValidations) => ({...prevValidations, "email": ""}))
+            }
+        }
+        if (e.target.name === 'password') {
+            if(e.target.value.length < 8) {
+                setValidations((prevValidations) => ({...prevValidations, "password": "Passwords must be 8 characters long and match each other."}))
+            } else if(e.target.value !== user.confirm) {
+                setValidations((prevValidations) => ({...prevValidations, "password": "Passwords must be 8 characters long and match each other."}))
+            } else {
+                setValidations((prevValidations) => ({...prevValidations, "password": ""}))
+            }
+        }
+        if(e.target.name ==='confirm') {
+            if(e.target.value !== user.password) {
+                setValidations((prevValidations) => ({...prevValidations, "password": "Passwords must be 8 characters long and match each other."}))
+            } else {
+                setValidations((prevValidations) => ({...prevValidations, "password": ""}))
+            }
+        }
+    }
     const updateUser = (e) => {
         setUser({...user, [e.target.name]: e.target.value})
+        errorChecker(e)
     }
 
     return <>
@@ -28,14 +75,17 @@ const Form = (props) => {
                 <label>First Name:</label>
                 <input name="firstName" type="text" onChange={updateUser} value={user.firstName}/>
             </div>
+            <p className="errorText">{validations.firstName}</p>
             <div>
                 <label>Last Name:</label>
                 <input name="lastName" type="text" onChange={updateUser} value={user.lastName}/>
             </div>
+            <p className="errorText">{validations.lastName}</p>
             <div>
                 <label>Email:</label>
                 <input name="email" type="text" onChange={updateUser} value={user.email}/>
             </div>
+            <p className="errorText">{validations.email}</p>
             <div>
                 <label>Password:</label>
                 <input name="password" type="password" onChange={updateUser} value={user.password}/>
@@ -44,6 +94,7 @@ const Form = (props) => {
                 <label>Confirm Password:</label>
                 <input name="confirm" type="password" onChange={updateUser} value={user.confirm}/>
             </div>
+            <p className="errorText">{validations.password}</p>
             <input type="submit" value="Create User"/>
         </form>
         <div className="formInfo">
