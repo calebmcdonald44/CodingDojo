b950bfc19e3f0cfad98d833d730fa360fdebf5e4
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2023-12-04 13:22:27 -0600

    updated to utilize axios

--- javaScript/react/pokemonAPI/src/Pokemon.jsx
+++ javaScript/react/pokemonAPI/src/Pokemon.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
+import axios from 'axios'
 
 function Pokemon() {
     const [pokemon, setPokemon] = useState([])
 
     const pokemonFetch = () => {
         console.log('clicked')
-        fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
             .then(response => {
-                console.log('response: ', response);
-                return response.json()
-            }).then(response => {
-                console.log('response: ', response);
-                setPokemon(response.results)
+                setPokemon(response.data.results)
             }).catch(err => {
-                console.log('err: ', err);
+                console.log('err: ', err)
             })
         }
+
 
     return (
         <>
