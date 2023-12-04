8a0a70a0ec7e0769f4dcb798069c675b3d2277d4
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2023-12-04 14:30:06 -0600

    reorganized to utilize lifting state

--- javaScript/react/todoList/src/App.jsx
+++ javaScript/react/todoList/src/App.jsx
@@ -1,19 +1,40 @@
 import { useState } from 'react'
 import './App.css'
 import Form from './Form.jsx'
-import Display from './Display.jsx'
+
 
 
 function App() {
   const [list, setList] = useState([
     {task: "Finish assignments", completed: false},
     {task: "Check fluids in car", completed: false},
-    {task: "Movie night with Max and Stoney", completed: false},
+    {task: "Eat dinner", completed: false},
   ])
+
+  const changeCompletion = (idx) => {
+    let newList = list.map((value, i) => idx != i ? value : {"task": value.task, completed: !value.completed})
+    setList(newList)
+  }
+
+  const deleteTask = (idx) => {
+    setList(list.filter((entry, i) => i != idx))
+  }
+  const addTask = (newTask) => {
+    setList([...list, newTask])
+}
+
+  const listMap = list.map(({task, completed}, idx) =>
+    <div key={idx} className={completed ? "completed" : "uncompleted"}>
+        <label htmlFor="">Task:</label>
+        <p>{task}</p>
+        <input type="checkbox" checked={completed} onChange={() => changeCompletion(idx)}/>
+        <button onClick={() => deleteTask(idx)}>Delete</button>
+    </div>
+  )
 
   return (
     <>
-      <Form list={list} setList={setList}></Form>
+      <Form changeCompletion={changeCompletion} deleteTask={deleteTask} listMap={listMap} addTask={addTask}></Form>
     </>
   )
 }
--- javaScript/react/todoList/src/Form.jsx
+++ javaScript/react/todoList/src/Form.jsx
@@ -1,26 +1,14 @@
 import { useState } from 'react'
 
-const Form = ({list, setList}) => {
+const Form = ({changeCompletion, deleteTask, listMap, addTask}) => {
 
     const [newTask, setNewTask] = useState()
 
-    const submitTask = (e) => {
-        taskTracker(e)
-        setList([...list, {task: newTask, completed: false}])
-    }
-
-    const changeCompletion = (idx) => {
-        let newList = []
-        list.map((value, i) => idx != i ? newList.push(value) : newList.push({"task": value.task, completed: !value.completed}))
-        setList(newList)
-    }
-
-    const deleteTask = (idx) => {
-        setList(list.filter((entry, i) => i != idx))
-    }
-
     const taskTracker = (e) => {
         setNewTask(e.target.value)
+    }
+    const sendTask = () => {
+        addTask({task: newTask, completed: false})
     }
 
     return (
@@ -28,18 +16,9 @@ const Form = ({list, setList}) => {
             <div className='flex'>
                 <label htmlFor="">Add a new task: </label>
                 <input type="text" onChange={(e) => taskTracker(e)}/>
-                <button onClick={(e) => submitTask(e)}>Add</button>
+                <button onClick={() => sendTask()}>Add</button>
             </div>
-            {
-                list.map(({task, completed}, idx) =>
-                    <div key={idx} className={completed ? "completed" : "uncompleted"}>
-                        <label htmlFor="">Task:</label>
-                        <p>{task}</p>
-                        <input id={idx} type="checkbox" defaultChecked={completed} onClick={(e) => changeCompletion(e.target.id)}/>
-                        <button id={idx} onClick={(e) => deleteTask(e.target.id)}>Delete</button>
-                    </div>
-                )
-            }
+            <>{listMap}</>
         </div>
     )
 }
