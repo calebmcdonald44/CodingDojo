3b91d1959dad60160a41132bda1cfb7f31a45c10
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2023-12-28 11:14:41 -0600

    practicing views/urls relationship in django

---
+++ python/django/first-django/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.root, name=""),
+    path('blogs/', views.index, name="blogs"),
+    path('blogs/new/', views.new, name="new"),
+    path('create/', views.create, name="create"),
+    path('blogs/<int:number>/', views.show, name="show"),
+    path('blogs/<int:number>/edit/', views.edit, name="edit"),
+    path('blogs/<int:number>/delete/', views.destroy, name="delete"),
+    path('blogs/json/', views.json, name="json")
+]
