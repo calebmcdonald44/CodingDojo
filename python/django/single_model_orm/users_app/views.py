a089719c3d49879c680b1aadc42eddaf38ae60a8
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2024-01-02 15:14:36 -0600

    html now displays info and form can add to db

--- python/django/single_model_orm/users_app/views.py
+++ python/django/single_model_orm/users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Users
 
-# Create your views here.
+def index(request):
+    users = Users.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request, 'index.html', context)
+
+def create(request):
+    user_data = {
+        'first_name': request.POST.get('first_name'),
+        'last_name': request.POST.get('last_name'),
+        'email_address': request.POST.get('email_address'),
+        'age': request.POST.get('age'),
+    }
+    new_user = Users.objects.create(**user_data)
+    return redirect('index')
