e92a945993ce30a9cb0fea80ebc6516d40f70108
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2024-01-03 10:10:43 -0600

    practice one to many rel/ORM commands in shell

---
+++ python/django/dojos-and-ninjas/dojos_ninjas_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DojosNinjasAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dojos_ninjas_app'
---
+++ python/django/dojos-and-ninjas/dojos_ninjas_app/urls.py
@@ -0,0 +1,6 @@
+from django.shortcuts import redirect, render
+from django.urls import path
+
+urlpatterns = [
+    # path()
+]
