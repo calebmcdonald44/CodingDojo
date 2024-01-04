7d92498908ef847af56764ed5cabf820d1465ee5
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2024-01-04 11:05:03 -0600

    templates now able to add/view books and authors

--- python/django/books-authors/books_authors_app/urls.py
+++ python/django/books-authors/books_authors_app/urls.py
@@ -1,2 +1,13 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('book/<int:book_id>/', views.view_book, name='view_book'),
+    path('book/<int:book_id>/add_author/', views.book_add_author, name='book_add_author'),
+    path('authors/', views.authors, name="authors"),
+    path('add_author/', views.add_author, name="add_authors"),
+    path('author/<int:author_id>/', views.view_author, name='view_author'),
+    path('author/<int:author_id>/add_book/', views.author_add_book, name='author_add_book'),
+]
--- python/django/books-authors/books_authors_app/views.py
+++ python/django/books-authors/books_authors_app/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
-# Create your views here.
+def index(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+    return render(request, 'index.html', context)
+
+def add_book(request):
+    new_title = request.POST.get('title')
+    new_desc = request.POST.get('desc')
+    Book.objects.create(title=new_title, desc=new_desc)
+    return redirect('index')
+
+def view_book(request, book_id):
+    current_book = Book.objects.get(id=book_id)
+    current_book_authors = current_book.authors.all()
+    authors = Author.objects.all()
+    available_authors = []
+
+    for a in authors:
+        include = True
+        for b in current_book_authors:
+            if b.id == a.id:
+                include = False
+        if include == True:
+            available_authors.append({'id': a.id, 'name': (a.first_name + ' ' + a.last_name)})
+
+    context = {
+        'book': current_book,
+        'authors': authors,
+        'available_authors': available_authors
+    }
+    return render(request, 'view_book.html', context)
+
+def book_add_author(request, book_id):
+    current_book = Book.objects.get(id=book_id)
+    current_author = Author.objects.get(id=request.POST.get('selected_author'))
+    current_book.authors.add(current_author)
+    return redirect(f'/book/{book_id}/')
+
+def authors(request):
+    authors = Author.objects.all()
+    context = {
+        'authors': authors
+    }
+    return render(request, 'authors.html', context)
+
+def add_author(request):
+    new_first_name = request.POST.get('first_name')
+    new_last_name = request.POST.get('last_name')
+    new_notes = request.POST.get('notes')
+    Author.objects.create(first_name=new_first_name, last_name=new_last_name, notes=new_notes)
+    return redirect('/authors/')
+
+def view_author(request, author_id):
+    current_author = Author.objects.get(id=author_id)
+    current_author_books = current_author.books.all()
+    books = Book.objects.all()
+    available_books = []
+
+    for a in books:
+        include = True
+        for b in current_author_books:
+            if b.id == a.id:
+                include = False
+        if include == True:
+            available_books.append({'id': a.id, 'title': (a.title)})
+
+    context = {
+        'author': current_author,
+        'books': books,
+        'available_books': available_books
+    }
+    return render(request, 'view_author.html', context)
+
+def author_add_book(request, author_id):
+    current_author = Author.objects.get(id=author_id)
+    current_book = Book.objects.get(id=request.POST.get('selected_book'))
+    current_author.books.add(current_book)
+    return redirect(f'/author/{author_id}/')
