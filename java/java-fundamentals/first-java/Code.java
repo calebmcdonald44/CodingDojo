33dead40865e8c44ddec376c0fa9f9bc10ff5e68
Caleb McDonald <caleb.scott.mcdonald@gmail.com>
2024-01-08 14:33:46 -0600

    first few Java assignments

---
+++ java/java-fundamentals/first-java/Code.java
@@ -0,0 +1,7 @@
+public class Code {
+    public static void main (String[] args) {
+        System.out.println("My name is Caleb McDonald");
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Overland Park, KS");
+    }
+}
